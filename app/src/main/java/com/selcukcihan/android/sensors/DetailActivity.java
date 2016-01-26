f971cf5469b4f46c3dde353144b350d6ff702274
selcukcihan <selcukcihan@gmail.com>
2016-01-26 21:15:33 +0200

    sharing

--- app/src/main/java/com/selcukcihan/android/sensors/DetailActivity.java
+++ app/src/main/java/com/selcukcihan/android/sensors/DetailActivity.java
@@ -6,11 +6,14 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
-import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -21,11 +24,48 @@ import java.util.Arrays;
 public class DetailActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor sensor;
+    private ShareActionProvider mShareActionProvider;
+    private Intent mShareIntent;
+    private String mShareString = "{0}{1}";
+    private String mSensorName = "";
+    private String mSensorValues = "";
     float []values;
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.details, popup.getMenu());
+        popup.show();
+    }
+
+    private void initializeShareIntent() {
+        mShareIntent = new Intent();
+        mShareIntent.setAction(Intent.ACTION_SEND);
+        mShareIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        mShareIntent.setType("text/plain");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.details, menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(mShareIntent);
+        }
+
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.custom_toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
@@ -35,14 +75,16 @@ public class DetailActivity extends AppCompatActivity implements SensorEventList
 
         Intent intent = getIntent();
         String sensorType = intent.getStringExtra(MainActivity.EXTRA_SENSOR_TYPE);
-        String sensorName = intent.getStringExtra(MainActivity.EXTRA_SENSOR_NAME);
+        mSensorName = intent.getStringExtra(MainActivity.EXTRA_SENSOR_NAME);
         String sensorImage = intent.getStringExtra(MainActivity.EXTRA_SENSOR_IMAGE);
 
         //getSupportActionBar().setTitle(sensorName);
 
         TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbar_title);
-        toolbarTitle.setText(sensorName);
+        toolbarTitle.setText(mSensorName);
         ((ImageView)findViewById(R.id.sensor_toolbar_icon)).setImageResource(Integer.parseInt(sensorImage));
+
+        //toolbar.inflateMenu(R.menu.details);
 
         sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Integer.parseInt(sensorType));
@@ -50,6 +92,9 @@ public class DetailActivity extends AppCompatActivity implements SensorEventList
         ((TextView)findViewById(R.id.sensor_name_vendor)).setText(sensor.getName() +  " {" + sensor.getVendor() + "}");
         //((TextView)findViewById(R.id.name)).setText(sensor.getName());
         //((TextView)findViewById(R.id.vendor)).setText(sensor.getVendor());
+
+        mShareString = getResources().getString(R.string.share_data);
+        initializeShareIntent();
     }
 
     protected void onResume() {
@@ -65,21 +110,18 @@ public class DetailActivity extends AppCompatActivity implements SensorEventList
     public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
 
     public void onSensorChanged(SensorEvent event) {
-        /*
-        Float [] boxed = new Float[event.values.length];
-        for (int i = 0; i < event.values.length; i++) {
-            boxed[i] = new Float(event.values[i]);
-        }
-        String values = TextUtils.join("-", boxed);
-        String formatted = getResources().getString(R.string.readings) + ": <" + values + ">";
-        ((TextView)findViewById(R.id.sensor_raw_values)).setText(formatted);*/
         values = event.values;
+        mSensorValues = Float.toString(values[0]);
+        mShareIntent.removeExtra(Intent.EXTRA_TEXT); // Remove previously set values from the intent
         ((TextView)findViewById(R.id.reading1_value)).setText(Float.toString(values[0]));
         if (values.length > 1) {
+            mSensorValues +=  " | " + Float.toString(values[1]);
             ((TextView)findViewById(R.id.reading2_value)).setText(Float.toString(values[1]));
             if (values.length > 2) {
+                mSensorValues +=  " | " + Float.toString(values[2]);
                 ((TextView)findViewById(R.id.reading3_value)).setText(Float.toString(values[2]));
             }
         }
+        mShareIntent.putExtra(Intent.EXTRA_TEXT, String.format(mShareString, mSensorName, mSensorValues));
     }
 }
