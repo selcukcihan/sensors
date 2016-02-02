9b8c9d712c261a2dc38d7c1385ad2ff00fffeb3e
selcukcihan <selcukcihan@gmail.com>
2016-02-02 08:29:09 +0200

    luminescence bug cozulmeye cok yakin

--- app/src/main/java/com/selcukcihan/android/sensors/LuminescenceFragment.java
+++ app/src/main/java/com/selcukcihan/android/sensors/LuminescenceFragment.java
@@ -1,27 +1,25 @@
 package com.selcukcihan.android.sensors;
 
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.components.LimitLine;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
-import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
 
 public class LuminescenceFragment extends SensorFragment {
 
-    private TextView mTextView;
-    private BarChart mChart;
+    private ImageView mImageView;
+    private GradientDrawable mGradient;
+    private double mMaxRange = 100;
 
     public LuminescenceFragment() {
         // Required empty public constructor
@@ -32,12 +30,23 @@ public class LuminescenceFragment extends SensorFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_luminescence, container, false);
-        //mChart = (BarChart) view.findViewById(R.id.luminescence_chart);
+        mImageView = (ImageView) view.findViewById(R.id.luminescence_circle);
+        mGradient = (GradientDrawable) mImageView.getDrawable();
+        mMaxRange = Math.log(mSensor.getSensor().getMaximumRange());
         return view;
     }
 
     @Override
     public void onRefresh(Object [] values) {
+        double value = Math.log(((Float) values[0]).doubleValue());
+        double ratio = value / mMaxRange;
+        if (ratio < 0) { // 0 lux ==> value == -infinity ==> ratio -ve ==> ratio cannot be -ve
+            ratio = 0;
+        } else if (ratio == 0) {
+            ratio = (Math.log(2) / mMaxRange) / 2;
+        }
 
+        float newRadius = (float)(mImageView.getWidth() * ratio);
+        mGradient.setGradientRadius(newRadius);
     }
 }
